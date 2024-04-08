093c5f7fd753ff132db85562c7d97e15db15b84a
dobri1408 <dobriceanionut1408@gmail.com>
2024-04-08 15:22:27 +0300

    improvments

--- index.js
+++ index.js
@@ -7,3 +7,36 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+// Get the button that opens the modal
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("closeModal");
+
+// When the user clicks on the button, open the modal
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+};
+
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+
+  e.preventDefault();
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  var modal = document.getElementById("myModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+function openModal(e) {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+
+  e.preventDefault();
+}
